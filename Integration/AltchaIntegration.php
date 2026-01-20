7591b20f6830bff3b24cebdcc7f624be926ffae4
Bjoern <bjoern_rafreider@visual4.de>
2026-01-20 11:14:54 +0100

    Fix ALTCHA configuration fields display using appendToForm
    - Changed from getAvailableFields() to appendToForm() method
    - Added NumberType and Range constraint imports
    - Fields maxNumber and expires now properly display in plugin configuration
    - Maintained validation constraints (1000-1000000 for maxNumber, 10-300 for expires)
    - Default values: maxNumber=50000, expires=120

--- Integration/AltchaIntegration.php
+++ Integration/AltchaIntegration.php
@@ -3,6 +3,8 @@
 namespace MauticPlugin\MauticMultiCaptchaBundle\Integration;
 
 use Mautic\PluginBundle\Integration\AbstractIntegration;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Validator\Constraints\Range;
 
 /**
  * <h1>Class AltchaIntegration</h1>
@@ -39,19 +41,52 @@ class AltchaIntegration extends AbstractIntegration {
     }
 
     /** {@inheritDoc} */
-    public function getAvailableFields() {
-        return [
-            "maxNumber" => [
+    public function appendToForm(&$builder, $data, $formArea): void {
+        if($formArea === "keys") {
+            $builder->add("maxNumber", NumberType::class, [
                 "label" => "strings.altcha.settings.max_number",
-                "type" => "number",
-                "required" => false
-            ],
-            "expires" => [
+                "required" => false,
+                "data" => isset($data["maxNumber"]) ? (int) $data["maxNumber"] : 50000,
+
+                "label_attr" => [
+                    "class" => "control-label"
+                ],
+
+                "attr" => [
+                    "class" => "form-control",
+                    "tooltip" => "strings.altcha.settings.max_number.tooltip"
+                ],
+
+                "constraints" => [
+                    new Range([
+                        "min" => 1000,
+                        "max" => 1000000,
+                        "notInRangeMessage" => "Value must be between {{ min }} and {{ max }}"
+                    ])
+                ]
+            ])->add("expires", NumberType::class, [
                 "label" => "strings.altcha.settings.expires",
-                "type" => "number",
-                "required" => false
-            ]
-        ];
+                "required" => false,
+                "data" => isset($data["expires"]) ? (int) $data["expires"] : 120,
+
+                "label_attr" => [
+                    "class" => "control-label"
+                ],
+
+                "attr" => [
+                    "class" => "form-control",
+                    "tooltip" => "strings.altcha.settings.expires.tooltip"
+                ],
+
+                "constraints" => [
+                    new Range([
+                        "min" => 10,
+                        "max" => 300,
+                        "notInRangeMessage" => "Value must be between {{ min }} and {{ max }}"
+                    ])
+                ]
+            ]);
+        }
     }
 
 }
